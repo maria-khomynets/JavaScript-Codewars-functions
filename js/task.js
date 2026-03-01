e668632a2832185ddf918461c3a5db9039052316
Maria Khomynets <masha.khomynets@gmail.com>
2026-03-01 08:44:51 +0200

    lesson-1

--- js/task.js
+++ js/task.js
@@ -32,53 +32,53 @@
 //   }
 // }
 
-for (let a = 10; a > 2; a--) {
-  if (a % 2 === 0) {
-    console.log(a);
-  }
-}
+// for (let a = 10; a > 2; a--) {
+//   if (a % 2 === 0) {
+//     console.log(a);
+//   }
+// }
 
-let sum = 0;
+// let sum = 0;
 
-for (let i = 1; i <= 5; i++) {
-  sum += i;
-}
+// for (let i = 1; i <= 5; i++) {
+//   sum += i;
+// }
 
-console.log(sum);
+// console.log(sum);
 
-for (let a = 0; a < 10; a++) {
-  console.log('число', a);
-}
+// for (let a = 0; a < 10; a++) {
+//   console.log('число', a);
+// }
 
-let stars = '';
-for (let i = 5; i > 0; i--) {
-  stars += '*';
-}
-console.log(stars);
-let rows = 5; // висота ялинки
+// let stars = '';
+// for (let i = 5; i > 0; i--) {
+//   stars += '*';
+// }
+// console.log(stars);
+// let rows = 5; // висота ялинки
 
-for (let i = 1; i <= rows; i++) {
-  let line = '';
+// for (let i = 1; i <= rows; i++) {
+//   let line = '';
 
-  // Додаємо пробіли перед зірочками для вирівнювання
-  for (let j = 1; j <= rows - i; j++) {
-    line += ' ';
-  }
+//   // Додаємо пробіли перед зірочками для вирівнювання
+//   for (let j = 1; j <= rows - i; j++) {
+//     line += ' ';
+//   }
 
-  // Додаємо зірочки: 1,3,5,7,...
-  for (let k = 1; k <= 2 * i - 1; k++) {
-    line += '*';
-  }
+//   // Додаємо зірочки: 1,3,5,7,...
+//   for (let k = 1; k <= 2 * i - 1; k++) {
+//     line += '*';
+//   }
 
-  console.log(line);
-}
+//   console.log(line);
+// }
 
-// Стовбур ялинки
-for (let i = 1; i <= 2; i++) {
-  let trunk = '';
-  for (let j = 1; j <= rows - 1; j++) {
-    trunk += ' ';
-  }
-  trunk += '|';
-  console.log(trunk);
-}
+// // Стовбур ялинки
+// for (let i = 1; i <= 2; i++) {
+//   let trunk = '';
+//   for (let j = 1; j <= rows - 1; j++) {
+//     trunk += ' ';
+//   }
+//   trunk += '|';
+//   console.log(trunk);
+// }
