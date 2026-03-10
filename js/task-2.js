e3e87dc831b4c47e6c187ce9c0bc154ca5b33bcf
Maria Khomynets <masha.khomynets@gmail.com>
2026-03-10 14:54:47 +0200

    task

--- js/task-2.js
+++ js/task-2.js
@@ -13,3 +13,20 @@ function getAllPropValues(propName) {
   console.log(arName);
 }
 getAllPropValues('name');
+
+function getProductPrice(productName) {
+  const products = [
+    { name: 'Radar', price: 1300, quantity: 4 },
+    { name: 'Scanner', price: 2700, quantity: 3 },
+    { name: 'Droid', price: 400, quantity: 7 },
+    { name: 'Grip', price: 1200, quantity: 9 },
+  ];
+  for (let i = 0; i < products.length; i++) {
+    console.log(products[i]);
+    console.log(products[i].name);
+    if (products[i].name === productName) {
+      return products[i].price;
+    }
+  }
+  return null;
+}
