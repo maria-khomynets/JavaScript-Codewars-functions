1ab6f40673827493a76af302e38cab588ac33757
Maria Khomynets <masha.khomynets@gmail.com>
2026-03-10 14:54:12 +0200

    task

--- js/task-2.js
+++ js/task-2.js
@@ -1,61 +1,15 @@
-// -------------------------------- 4
-
-/**
- * Напиши функцію logItems(items), яка отримує масив та використовує цикл,
- * який для кожного елемента масиву буде виводити в консоль повідомлення у
- * форматі <номер елемента> - <значення елемента>. Нумерація елементів
- * повинна починатися з 1.
- *
- * Наприклад для першого елемента масиву ['Mango', 'Poly', 'Ajax'] з
- * індексом 0 буде виведено 1 - Mango, а для індексу 2 виведе 3 - Ajax.
- */
-
-function logItems(arr) {
-  let ar = [];
-  for (let i = 0; i < arr.length; i++) {
-    console.log(`${i + 1} - ${arr[i]}`);
+function getAllPropValues(propName) {
+  const products = [
+    { name: 'Radar', price: 1300, quantity: 4 },
+    { name: 'Scanner', price: 2700, quantity: 3 },
+    { name: 'Droid', price: 400, quantity: 7 },
+    { name: 'Grip', price: 1200, quantity: 9 },
+  ];
+  let arName = [];
+  for (const name of products) {
+    arName.push(name.products);
+    console.log(name);
   }
+  console.log(arName);
 }
-
-console.log(logItems(['🍎', '🍇', '🍑', '🍌', '🍋']));
-logItems(['Mango', 'Poly', 'Ajax']);
-
-/**
- * Напиши функцію printInfo(names, phones) яка виводить
- * у консоль ім'я та телефонний номер користувача. У параметри
- * names та phones будуть передані рядки імен та телефонних номерів,
- * розділені комами. Порядковий номер імен та телефонів у рядках
- * вказують на відповідність. Кількість імен та телефонів
- * гарантовано однакова.
- */
-
-// function printInfo(names, phones) {
-//   const name = names.split(',');
-//   const phone = phones.split(',');
-
-//   for (let i = 0; i < name.length; i++) {
-//     console.log(name[i], phone[i]);
-//   }
-// }
-// printInfo(
-//   'Jacob,William,Solomon,Artemis',
-//   '89001234567,89001112233,890055566377,890055566300'
-// );
-
-/**
- * Напиши функцію formatTime(totalMinutes) яка переведе значення
- * totalMinutes (кількість хвилин) у рядок у форматі годин
- * та хвилин HH:MM.
- */
-
-function formatTime(totalMinutes) {
-  const hours = Math.floor(totalMinutes / 60);
-  const minute = totalMinutes % 60;
-  console.log(hours);
-  console.log(minute);
-  console.log(`${hours}:${minute}`);
-}
-formatTime(70);
-// console.log(formatTime(70)); // 1:10
-// console.log(formatTime(460));
-// console.log(formatTime(1441));
+getAllPropValues('name');
