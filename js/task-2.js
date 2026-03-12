f4c4a5ec08aaa1dff640ec84c10dd423dd618a0a
Maria Khomynets <masha.khomynets@gmail.com>
2026-03-12 11:58:51 +0200

    tash

--- js/task-2.js
+++ js/task-2.js
@@ -35,11 +35,12 @@ function isEnoughCapacity(products, containerSize) {
   for (const key in products) {
     total += products[key];
   }
-  if (total < containerSize) {
-    return true;
-  } else {
-    return false;
-  }
+  return total < containerSize;
+  // if (total < containerSize) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
 }
 
 console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
@@ -49,3 +50,46 @@ console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
 console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
 
 console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
+
+// Напиши функцію calcAverageCalories(days), яка повертає середньодобове значення кількості калорій,
+//   які спортсмен споживав протягом тижня.
+//   Функція очікує один параметр: days — масив об’єктів.Кожен об’єкт описує день тижня та кількість калорій calories,
+//   спожитих спортсменом, у цей день.Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи.
+// У консоль будуть виведені результати її викликів.
+
+function calcAverageCalories(days) {
+  let total = 0;
+  if (days.length === 0) {
+    return total;
+  }
+  for (const item of days) {
+    total += item.calories;
+  }
+  return total / days.length;
+}
+
+console.log(
+  calcAverageCalories([
+    { day: 'monday', calories: 3010 },
+    { day: 'tuesday', calories: 3200 },
+    { day: 'wednesday', calories: 3120 },
+    { day: 'thursday', calories: 2900 },
+    { day: 'friday', calories: 3450 },
+    { day: 'saturday', calories: 3280 },
+    { day: 'sunday', calories: 3300 },
+  ])
+); // 3180
+
+console.log(
+  calcAverageCalories([
+    { day: 'monday', calories: 2040 },
+    { day: 'tuesday', calories: 2270 },
+    { day: 'wednesday', calories: 2420 },
+    { day: 'thursday', calories: 1900 },
+    { day: 'friday', calories: 2370 },
+    { day: 'saturday', calories: 2280 },
+    { day: 'sunday', calories: 2610 },
+  ])
+); // 2270
+
+console.log(calcAverageCalories([])); // 0
