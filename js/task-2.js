4b33d3f8b3c6a175155a23195b327291052263b1
Maria Khomynets <masha.khomynets@gmail.com>
2026-03-12 10:18:04 +0200

    task

--- js/task-2.js
+++ js/task-2.js
@@ -1,39 +1,48 @@
-function назваФункції(аргумент) {
-  // 1. "Що я маю отримати в кінці?"
-  // (Масив значень? Число-суму? Один об'єкт?)
-  // -> Створюємо "кошик": let result = []; або let total = 0;
-  // 2. "Як мені взяти в руки кожен елемент масиву?"
-  // -> Пишемо цикл: for (const item of array) {
-  // 3. "Чи всі елементи мені підходять?"
-  // (Чи є в об'єкті такий ключ? Чи ціна більша за 100?)
-  // -> Пишемо умову: if (умова) {
-  // 4. "Що саме я хочу забрати з цього елемента?"
-  // (Весь об'єкт чи тільки одне його поле?)
-  // -> Додаємо в кошик: result.push(item[propName]);
-  // } (закриваємо if)
-  // } (закриваємо for)
-  // 5. "Я не забув віддати те, що зібрав?"
-  // -> Пишемо: return result;
-}
+//function назваФункції(аргумент) {
+// 1. "Що я маю отримати в кінці?"
+// (Масив значень? Число-суму? Один об'єкт?)
+// -> Створюємо "кошик": let result = []; або let total = 0;
+// 2. "Як мені взяти в руки кожен елемент масиву?"
+// -> Пишемо цикл: for (const item of array) {
+// 3. "Чи всі елементи мені підходять?"
+// (Чи є в об'єкті такий ключ? Чи ціна більша за 100?)
+// -> Пишемо умову: if (умова) {
+// 4. "Що саме я хочу забрати з цього елемента?"
+// (Весь об'єкт чи тільки одне його поле?)
+// -> Додаємо в кошик: result.push(item[propName]);
+// } (закриваємо if)
+// } (закриваємо for)
+// 5. "Я не забув віддати те, що зібрав?"
+// -> Пишемо: return result;
 
-// Умова: Напиши функцію getMostExpensiveProduct(products).
-// Вона має пройтися по масиву і повернути весь об'єкт товару, у якого найвища ціна.
-//
-//
-
-const store = [
-  { name: 'Laptop', price: 30000 },
-  { name: 'Smartphone', price: 150000 },
-  { name: 'Tablet', price: 20000 },
+// Умова: Напиши функцію getCheapestAvailableProduct(products).
+// Вона має повернути об'єкт товару, який має найменшу ціну, але при цьому його кількість (quantity) більша за 0.
+const stock = [
+  { name: 'Case', price: 200, quantity: 0 },
+  { name: 'Fan', price: 500, quantity: 5 },
+  { name: 'Cable', price: 300, quantity: 0 },
+  { name: 'Power Bank', price: 1200, quantity: 2 },
 ];
-function getMostExpensiveProduct(products) {
-  let result = products[0];
 
-  for (const item of products) {
-    if (item.price > result.price) {
-      result = item;
+function getCheapestAvailableProduct(products) {
+  let minProduct = {};
+
+  for (const product of products) {
+    const min = minProduct.price;
+    if (min === undefined) {
+      if (product.quantity > 0) {
+        minProduct = product;
+      }
+    }
+    if (min > product.price && product.quantity > 0) {
+      minProduct = product;
     }
   }
-  return result;
+  return minProduct;
 }
-console.log(getMostExpensiveProduct(store));
+console.log(getCheapestAvailableProduct(stock));
+
+// const result = getCheapestAvailableProduct(stock);
+// console.log('>>>function result:::', result);
+
+// Очікуваний результат: { name: 'Fan', price: 500, quantity: 5 }
