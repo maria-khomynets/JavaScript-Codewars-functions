9ad57181c6c2bb60cd1f62ce57340f47059c2277
Maria Khomynets <masha.khomynets@gmail.com>
2026-03-21 13:53:01 +0200

    this

--- js/lesson-02.js
+++ js/lesson-02.js
@@ -220,19 +220,13 @@
 //   }
 // }
 
-const pizzaPalace = {
-  pizzas: ['Supercheese', 'Smoked', 'Four meats'],
-  checkPizza(pizzaName) {
-    return this.pizzas.includes(pizzaName);
-  },
-  order(pizzaName) {
-    const isPizzaAvailable = this.checkPizza(pizzaName);
-
-    if (!isPizzaAvailable) {
-      return `Sorry, there is no pizza named «${pizzaName}»`;
-    }
-
-    return `Order accepted, preparing «${pizzaName}» pizza`;
+const bear = {
+  name: 'Бобо',
+  name2: 'Бобо2',
+  sayName() {
+    console.log(this.name + this.name2);
   },
 };
-console.log(pizzaPalace.order('Smoked'));
+
+bear.sayName();
+bear.name;
