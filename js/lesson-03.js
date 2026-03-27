a34201f0ed284d6d0d254b359cc1c5c32f9b7cff
Maria Khomynets <masha.khomynets@gmail.com>
2026-03-27 15:36:02 +0200

    lesson

--- js/lesson-03.js
+++ js/lesson-03.js
@@ -5,3 +5,31 @@
 const numbers = [1, 2, 3, 4, 5];
 const numbersMap = numbers.map(item => item * item);
 console.log(numbersMap);
+
+// Дано масив об'єктів. Створіть новий масив, що містить всі значення
+// з масивів values кожного об'єкту, збережених в одному масиві.
+// Очікуваний результат: [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+const data = [
+  { id: 1, values: [1, 2, 3] },
+  { id: 2, values: [4, 5, 6] },
+  { id: 3, values: [7, 8, 9] },
+];
+
+const dataM = data.flatMap(item => item.values);
+console.log(dataM);
+
+// Дано масив об'єктів.
+// Перевірте, чи є хоча б один об'єкт з віком менше 20 років.
+// Очікуваний результат: true.
+
+const people = [
+  { name: 'John', age: 27 },
+  { name: 'Jane', age: 31 },
+  { name: 'Bob', age: 19 },
+];
+
+// const peopleF = people.find(item => item.age < 20);
+// console.log(peopleF);
+const result = people.some(item => item.age < 20);
+console.log(result);
