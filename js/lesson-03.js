746e52662403aefdec0e8a3eef13d75652cae0ae
Maria Khomynets <masha.khomynets@gmail.com>
2026-03-28 12:18:33 +0200

    hw

--- js/lesson-03.js
+++ js/lesson-03.js
@@ -47,4 +47,6 @@
 
 // Знайдіть перше непарне число
 
-const numbers = [2, 1, 6, 8, 9, 10, 12];
+const numbers = [2, 10, 6, 8, 9, 10, 12];
+const numbersFind = numbers.find(item => item % 2 !== 0);
+console.log(numbersFind);
