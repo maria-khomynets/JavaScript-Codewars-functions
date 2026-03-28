19b87fc489d9de1fc25897f1d52e65c70b6b0119
Maria Khomynets <masha.khomynets@gmail.com>
2026-03-28 12:12:01 +0200

    const

--- js/lesson-03.js
+++ js/lesson-03.js
@@ -1,129 +1,50 @@
 // Дано масив чисел [1, 2, 3, 4, 5]. Створіть новий масив, що містить
-//   квадрати кожного елементу вхідного масиву.
-//  Очікуваний результат: [1, 4, 9, 16, 25].
-//
-const numbers = [1, 2, 3, 4, 5];
-const numbersMap = numbers.map(item => item * item);
-console.log(numbersMap);
+// квадрати кожного елементу вхідного масиву.
+// Очікуваний результат: [1, 4, 9, 16, 25].
+
+// const numbers = [1, 2, 3, 4, 5];
+// const numbersMap = numbers.map(item => item * item);
+// console.log(numbersMap);
 
 // Дано масив об'єктів. Створіть новий масив, що містить всі значення
 // з масивів values кожного об'єкту, збережених в одному масиві.
 // Очікуваний результат: [1, 2, 3, 4, 5, 6, 7, 8, 9].
 
-const data = [
-  { id: 1, values: [1, 2, 3] },
-  { id: 2, values: [4, 5, 6] },
-  { id: 3, values: [7, 8, 9] },
-];
+// const data = [
+//   { id: 1, values: [1, 2, 3] },
+//   { id: 2, values: [4, 5, 6] },
+//   { id: 3, values: [7, 8, 9] },
+// ];
 
-const dataM = data.flatMap(item => item.values);
-console.log(dataM);
+// const dataflatMap = data.flatMap(item => item.values);
+// console.log(dataflatMap);
 
 // Дано масив об'єктів.
 // Перевірте, чи є хоча б один об'єкт з віком менше 20 років.
 // Очікуваний результат: true.
 
-const people = [
-  { name: 'John', age: 27 },
-  { name: 'Jane', age: 31 },
-  { name: 'Bob', age: 19 },
-];
+// const people = [
+//   { name: 'John', age: 27 },
+//   { name: 'Jane', age: 31 },
+//   { name: 'Bob', age: 19 },
+// ];
 
-// const peopleF = people.find(item => item.age < 20);
-// console.log(peopleF);
-const result = people.some(item => item.age < 20);
-console.log(result);
+// const result = people.some(item => item.age < 20);
+// console.log(result);
 
 // Дано масив чисел [2, 4, 6, 8, 10].
 // Перевірте, чи є кожен елемент масиву парним. Очікуваний результат: true.
 
-const numbers1 = [2, 4, 6, 8, 10];
-const result1 = numbers1.every(item => item % 2 === 0);
-console.log(result1);
+// const numbers = [2, 4, 6, 7, 8, 10];
+
+// const numbersEvery = numbers.every(item => item % 2 === 0);
+// console.log(numbersEvery);
+
+// const numbersEvery1 = numbers.reduce((acc, item) => {
+//   return acc && item % 2 === 0;
+// }, true);
+// console.log(numbersEvery1);
 
 // Знайдіть перше непарне число
 
-const numbers2 = [2, 1, 6, 8, 9, 10, 12];
-
-const numbers2W = numbers2.find(item => item % 2 !== 0);
-console.log(numbers2W);
-
-// Відсортуйте масив чисел [4, 2, 5, 1, 3]
-// у порядку зростання. Очікуваний результат: [1, 2, 3, 4, 5].
-
-const numbersArray = [4, 2, 5, 1, 3];
-const numbersArrayS = numbersArray.toSorted((a, b) => a - b);
-console.log(numbersArrayS);
-
-// Відсортуйте масив рядків ["banana", "orange", "apple", "pear"]
-//  у порядку алфавіту.
-// Очікуваний результат: ["apple", "banana", "orange", "pear"].
-
-const stringArray = ['banana', 'orange', 'apple', 'pear'];
-
-const stringArrayS = stringArray.toSorted((a, b) => a.localeCompare(b));
-console.log(stringArrayS);
-
-//  Відсортуйте масив об'єктів за віком у порядку зростання.
-// Очікуваний результат: [{name: "Bob", age: 19}, {name: "John", age: 27}, {name: "Jane", age: 31}].
-
-const users = [
-  { name: 'John', age: 27 },
-  { name: 'Jane', age: 31 },
-  { name: 'Bob', age: 19 },
-];
-
-const usersSorted = users.toSorted((a, b) => a.age - b.age);
-console.log(usersSorted);
-
-// Дано масив об'єктів.
-// Створіть новий масив, що містить тільки об'єкти, в яких
-// вік більше 20 років.
-// Очікуваний результат: [{name: "John", age: 27}, {name: "Jane", age: 31}]
-
-const user1 = [
-  { name: 'John', age: 27 },
-  { name: 'Jane', age: 31 },
-  { name: 'Bob', age: 19 },
-];
-
-const rez = user1.filter(item => item.age > 20);
-console.log(rez);
-
-// Дано масив чисел [1, 2, 3, 4, 5].
-// Застосуйте метод для обчислення суми елементів масиву.
-
-const numbers3 = [1, 2, 3, 4, 5, 6];
-const rez1 = numbers3.reduce((acc, num) => {
-  return acc + num;
-}, 0);
-console.log(rez1); //  операції над числом за допомогою методів класу, підтримуючи
-// ланцюжковий виклик (method chaining).
-//
-// Вимоги до класу Calculator
-// - Метод number(value)
-// Встановлює початкове значення для наступних обчислень.
-// Повертає сам об'єкт (this) для підтримки ланцюжкових викликів.
-// - метод getResult, Повертає поточний результат усіх операцій.
-// Не змінює значення, просто повертає його.
-// - метод add - Додає value до поточного значення.
-// Повертає сам об'єкт (this) для підтримки ланцюжкових викликів.
-// - метод subtract - Віднімає value від поточного значення. Повертає this.
-// - метод multiply -Множить поточне значення на value. Повертає this.
-// Об'єкт класу може проводити послідовні операції у вигляді ланцюжка
-// Приклад використання:
-// const calc = new Calculator();
-
-// Розроби клас Calculator, який дозволяє виконувати арифметичні
-// - метод divide - Ділить поточне значення на value, якщо value не дорівнює 0.
-// Якщо value === 0, викидає помилку з повідомленням про неможливість ділення.
-// Повертає сам об'єкт (this) для підтримки ланцюжкових викликів.
-const result = calc
-  .number(10) // Встановлюємо початкове значення 10
-  .add(5) // Додаємо 5 (10 + 5 = 15)
-  .subtract(3) // Віднімаємо 3 (15 - 3 = 12)
-  .multiply(4) // Множимо на 4 (12 * 4 = 48)
-  .divide(2) // Ділимо на 2 (48 / 2 = 24)
-  .getResult(); // Отримуємо результат: 24
-
-console.log(result); // 24
+const numbers = [2, 1, 6, 8, 9, 10, 12];
--- js/task-2.js
+++ js/task-2.js
@@ -94,50 +94,50 @@
 
 // console.log(calcAverageCalories([])); // 0
 
-const students = [
-  { name: 'Mango', score: 83, courses: ['mathematics', 'physics'] },
-  { name: 'Poly', score: 59, courses: ['science', 'mathematics'] },
-  { name: 'Ajax', score: 37, courses: ['physics', 'biology'] },
-  { name: 'Kiwi', score: 94, courses: ['literature', 'science'] },
-];
+// const students = [
+//   { name: 'Mango', score: 83, courses: ['mathematics', 'physics'] },
+//   { name: 'Poly', score: 59, courses: ['science', 'mathematics'] },
+//   { name: 'Ajax', score: 37, courses: ['physics', 'biology'] },
+//   { name: 'Kiwi', score: 94, courses: ['literature', 'science'] },
+// ];
 
-const names = students
-  .toSorted((a, b) => a.score - b.score)
-  .map(student => student.name);
+// const names = students
+//   .toSorted((a, b) => a.score - b.score)
+//   .map(student => student.name);
 
-console.log(names); // ["Ajax", "Poly", "Mango", "Kiwi"]
+// console.log(names); // ["Ajax", "Poly", "Mango", "Kiwi"]
 
-const books = [
-  {
-    title: 'The Last Kingdom',
-    author: 'Bernard Cornwell',
-    rating: 8.38,
-  },
-  {
-    title: 'Beside Still Waters',
-    author: 'Robert Sheckley',
-    rating: 8.51,
-  },
-  {
-    title: 'The Tell-Tale Heart',
-    author: 'Edgar Allan Poe',
-    rating: 7.75,
-  },
-  { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
-  {
-    title: 'The Dreams in the Witch House',
-    author: 'Howard Lovecraft',
-    rating: 8.67,
-  },
-];
-const MIN_BOOK_RATING = 8;
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     rating: 8.38,
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     rating: 8.51,
+//   },
+//   {
+//     title: 'The Tell-Tale Heart',
+//     author: 'Edgar Allan Poe',
+//     rating: 7.75,
+//   },
+//   { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+//   {
+//     title: 'The Dreams in the Witch House',
+//     author: 'Howard Lovecraft',
+//     rating: 8.67,
+//   },
+// ];
+// const MIN_BOOK_RATING = 8;
 
-const nameD = books
-  .filter(value => value.rating >= MIN_BOOK_RATING)
-  .map(nameBook => nameBook.author)
-  .toSorted((a, b) => a.localeCompare(b));
+// const nameD = books
+//   .filter(value => value.rating >= MIN_BOOK_RATING)
+//   .map(nameBook => nameBook.author)
+//   .toSorted((a, b) => a.localeCompare(b));
 
-console.log(nameD);
+// console.log(nameD);
 
 // вибрати масив
 //відфільтрувати
